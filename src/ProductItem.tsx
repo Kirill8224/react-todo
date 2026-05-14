4eb047cbf1b4402099b95e8b38957df93abb9832
Kirill Tatarenko <Kirilltatarenko97@gmail.com>
2026-05-14 08:32:41 +0300

    beta todo

---
+++ src/ProductItem.tsx
@@ -0,0 +1,5 @@
+export function ProductItem(props){
+    return(<div>
+        <p>{props}</p>
+    </div>)
+}
---
+++ src/ProductList.tsx
@@ -0,0 +1,4 @@
+import { ProductItem } from "./ProductItem"
+export function ProductList(){
+    return(ProductItem(1))
+}
